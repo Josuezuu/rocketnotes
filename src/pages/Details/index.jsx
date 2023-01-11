e4a0493a95f94256fb6dd0f9254627c29760f16c
josue <josue-zu@hotmail.com>
2023-01-10 23:20:29 -0300

    first page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,25 @@
-import {Container, Links} from './styles'
+import {Container, Links, Content} from './styles'
 import {Header} from '../../components/Header'
 import {Button} from '../../components/Button'
 import {Section} from '../../components/Section'
+import {Tag} from '../../components/Tag'
+import {ButtonText} from '../../components/ButtonText'
+
 
 export function Details(){
   
   return(
     <Container>
       <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio praesentium quidem rem quasi consequuntur ipsa explicabo, natus ad esse rerum aut sed voluptate aliquam, provident, labore eligendi sapiente ipsam voluptatibus?
+          </p>
       <Section title="Links úteis">
         <Links>
           <li><a href='https://github.com/Josuezuu' target="_blank">https://github.com/Josuezuu</a></li>
@@ -17,11 +29,14 @@ export function Details(){
       </Section>
 
       <Section title="Marcadores">
-
+        <Tag title="Express"/>
+        <Tag title="Node"/>
       </Section>
 
 
       <Button title="Voltar" />
+      </Content>
+      </main>
     </Container>
     ) 
 }
