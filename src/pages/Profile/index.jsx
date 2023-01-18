af18c24f6f1519ce779b2b42b2165378dbc81e33
josue <josue-zu@hotmail.com>
2023-01-18 00:36:50 -0300

    signup

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import { FiArrowLeft } from "react-icons/fi";
+import { Container } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <a href="/">
+        <FiArrowLeft />
+      </a>
+    </Container>
+  )
+}
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,18 +1,24 @@
 import { Container, Form, Background } from "./styles";
 import { Input } from "../../components/Input";
-import { FiMail, FiLock} from 'react-icons/fi';
+import { FiMail, FiLock, FiUser} from 'react-icons/fi';
 import { Button } from "../../components/Button";
 
 
-export function SignIn(){
+export function SignUp(){
   return (
     <Container>
+            <Background />
+
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login:</h2>
-
+        <h2>Crie sua conta:</h2>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
         <Input
           placeholder="Email"
           type="text"
@@ -23,12 +29,11 @@ export function SignIn(){
           type="text"
           icon={FiLock}
         />
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
-        <a href="#">Criar conta</a>          
+        <a href="#">Voltar para o login</a>          
 
       </Form>
-      <Background />
 
     </Container>
   );
