2b470bc8ba81dad409e5e8279c5352504ac4e96a
josue <josue-zu@hotmail.com>
2023-02-08 22:54:17 -0300

    cadastro de usuario ok

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,7 +3,7 @@ import { Container, Form, Background } from "./styles";
 import { Input } from "../../components/Input";
 import { FiMail, FiLock, FiUser} from 'react-icons/fi';
 import { Button } from "../../components/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { api } from "../../services/api"
 
@@ -12,6 +12,8 @@ export function SignUp(){
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const navigate = useNavigate();
+
   function handleSignUp(){
     if(!name || !email || !password){
       return alert("Preencha todos os campos!");
@@ -19,10 +21,11 @@ export function SignUp(){
     api.post("/users", {name, email, password})
     .then(() => {
       alert("Usuário cadastrado com sucesso!");
+      navigate("/");
     })
     .catch(error => {
       if(error.response){
-        alert("error.response.data.message")
+        alert(error.response.data.message)
       }else{
         alert("Não foi possível cadastrar")
       }
